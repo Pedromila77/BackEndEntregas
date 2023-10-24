f5ea71f024851816d83fe779e2eef2dc9dd777af
Pedromila77 <pedromilaing77@gmail.com>
2023-10-23 21:46:14 -0300

    First Commit

---
+++ 01/01-entregable.js
@@ -0,0 +1,65 @@
+class ProductManager {
+
+    //CADA CONSTRUCTOR CON SU CODE INICIAL EN 1
+    constructor() {
+        this.products = [];
+      
+    }
+
+
+    //UN METODO QUE TE RETORNE EL ARRAY DE PRODUCTOS
+    getProducts() {
+        return this.products;
+    }
+    //UN METODO QUE RECIBE LOS PARAMETROS PEDIDOS Y UN INCREMENTADOR POR CADA CODE 
+    addProduct(title, description, price, thumbnail, stock) {
+        
+        let code=1 
+        if (this.products.length > 0){
+            code=this.products[this.products.length - 1].code + 1
+        }
+        
+        
+        let newProduct = {
+            code,
+            title,
+            description,
+            price,
+            thumbnail,
+            stock,
+        };
+        
+        this.products.push(newProduct);
+    }
+    //SE DECLARA UN METODO DONDE SE HACE UN FIND POR ID DE ITEM QUE RETORNE EL ARRAY DE UNO DE LOS PRODUCTOS EN EL CASO DE QUE EXISTA 
+    getProductById(code){
+
+        if(!this.products.find((products)=> products.code === code)){
+        
+        return `Not Found`
+        
+        }else{
+        
+        return this.products.find((products)=> products.code === code)
+        
+        }
+        
+        }
+}
+//DECLARO 2 PRODUCTOS Y LOS MUESTRO POR CONSOLA
+let productManager = new ProductManager();
+productManager.addProduct(`Gallo Oro`, `Arroz Fino`, `$500`, `Sin Imagen`, `20`);
+productManager.addProduct(`Amanda`, `Arroz`, `$400`, `Sin Imagen`, `10`);
+
+console.log(productManager.getProducts());
+
+
+console.log(productManager.getProductById(1));
+console.log(productManager.getProductById(2));
+console.log(productManager.getProductById(3));
+
+
+
+
+
+
